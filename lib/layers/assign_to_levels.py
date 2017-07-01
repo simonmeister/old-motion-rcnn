a03d8d0367e7c4d2ae2bbd425ee862b191b57a33
Simon Meister <simon.meister.93@gmail.com>
2017-07-01 20:25:06 +0200

    add level assignment test

--- lib/layers/assign_to_levels.py
+++ lib/layers/assign_to_levels.py
@@ -9,13 +9,14 @@ import numpy as np
 import math
 
 
-def assign_to_levels(boxes, im_size, num_levels=6, finest_stride=4):
+def assign_to_levels(boxes, im_size, num_levels=6, finest_stride=3):
     """Assigns boxes to pyramid levels.
 
     Args:
         boxes: array of shape (N, 4), [[x1, y1, x2, y2], ...]
         im_size: [height, width] of full image
         num_levels: number of pyramid levels
+        finest_stride: average stride of box side length
 
     Returns:
         level_ids: array of shape (N,), per-box level indices,
@@ -27,7 +28,7 @@ def assign_to_levels(boxes, im_size, num_levels=6, finest_stride=4):
     areas = widths * heights
     # if e.g. the finest level has a stride of 4, we want all boxes
     # at 1/4 image resolution to map to the coarsest level (k = 0)
-    k = np.round(np.log2(im_area / areas) - math.log2(finest_stride))
+    k = np.round(np.log2(math.sqrt(im_area) / np.sqrt(areas)) - math.log2(finest_stride))
     k = k.astype(np.int32)
     inds = np.where(k < 0)[0]
     k[inds] = 0
